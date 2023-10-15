1a46e28b863bc4e4263aac68aa7c37da9c4285d5
akramab <13519142@std.stei.itb.ac.id>
2023-10-15 13:29:17 +0700

    feat: add logging

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,40 +21,42 @@ class UserController extends Controller
         error_log('BEFORE FOREACH');
         error_log(count($users));
         foreach($users as $user) {
-            error_log('INSIDE FOREACH');
-            $institutionName = '';
-            if ($user->competition_type == 'ISOTERM') {
-                error_log('INSIDE IF INS NAME');
-                $institutionName = $user->profile->getInstitutionData()->university->name;
-            } else {
-                error_log('INSIDE ELSE INS NAME');
-                $institutionName = $user->profile->getInstitutionData()->school->name;
-            }
-            $institutionInstructor = '';
-            if ($user->competition_type == 'ISOTERM') {
-                $institutionInstructor = $user->profile->getInstitutionData()->university->lecturer;
-            } else {
-                $institutionInstructor = $user->profile->getInstitutionData()->teacher->name;
-            }
+            if ($user->profile != null) {
+                error_log('INSIDE FOREACH');
+                $institutionName = '';
+                if ($user->competition_type == 'ISOTERM') {
+                    error_log('INSIDE IF INS NAME');
+                    $institutionName = $user->profile->getInstitutionData()->university->name;
+                } else {
+                    error_log('INSIDE ELSE INS NAME');
+                    $institutionName = $user->profile->getInstitutionData()->school->name;
+                }
+                $institutionInstructor = '';
+                if ($user->competition_type == 'ISOTERM') {
+                    $institutionInstructor = $user->profile->getInstitutionData()->university->lecturer;
+                } else {
+                    $institutionInstructor = $user->profile->getInstitutionData()->teacher->name;
+                }
 
-            error_log('AFTER ASSIGNEMNT INSIDE FOR EACH');
-            error_log($institutionName);
-            error_log($institutionInstructor);
+                error_log('AFTER ASSIGNEMNT INSIDE FOR EACH');
+                error_log($institutionName);
+                error_log($institutionInstructor);
 
-            $data[] = [
-                'id' => $user->id,
-                'verified' => $user->is_payment_verified,
-                'category' => $user->competition_type,
-                'date' => $datetime->format(DateTimeInterface::ATOM),
-                'name' => [
-                    'team' => $user->profile->team,
-                    'member' => $user->profile->getMembersData()[0]->name,
-                ],
-                'institution' => [
-                    'name' => $institutionName,
-                    'instructor' => $institutionInstructor
-                ]
-            ];
+                $data[] = [
+                    'id' => $user->id,
+                    'verified' => $user->is_payment_verified,
+                    'category' => $user->competition_type,
+                    'date' => $datetime->format(DateTimeInterface::ATOM),
+                    'name' => [
+                        'team' => $user->profile->team,
+                        'member' => $user->profile->getMembersData()[0]->name,
+                    ],
+                    'institution' => [
+                        'name' => $institutionName,
+                        'instructor' => $institutionInstructor
+                    ]
+                ];
+            }
         }
         error_log('BEFORE RETURN');
         return response()->json($data);
