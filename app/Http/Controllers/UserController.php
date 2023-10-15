7e819307221dfbec998fd3b25ae83f5c5da8c031
akramab <13519142@std.stei.itb.ac.id>
2023-10-15 10:57:09 +0700

    feat: add get user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DateTime;
+use DateTimeInterface;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(Request $request) {
+        $datetime = new DateTime('2010-12-30 23:21:46');
+        return response()->json([
+            [
+                'id' => 1,
+                'verified' => true,
+                'date' => $datetime->format(DateTimeInterface::ATOM),
+                'name' => [
+                    'team' => 'Tim Elang',
+                    'member' => 'Rudi Hermansyah',
+                ],
+                'institution' => [
+                    'name' => 'ITB',
+                    'instructor' => 'Surendro'
+                ]
+            ],
+            [
+                'id' => 2,
+                'verified' => true,
+                'date' => $datetime->format(DateTimeInterface::ATOM),
+                'name' => [
+                    'team' => 'Tim Rajawali',
+                    'member' => 'Budi Suradi',
+                ],
+                'institution' => [
+                    'name' => 'ITS',
+                    'instructor' => 'Kridanto'
+                ]
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/user', 'index')->name('user.get-list');
+    });
+
     Route::controller(ProfileController::class)->group(function () {
         Route::post('/user/profile', 'updateOrCreate')->name('user.update-or-create-profile');
         Route::get('/user/profile', 'get')->name('user.get-profile');
